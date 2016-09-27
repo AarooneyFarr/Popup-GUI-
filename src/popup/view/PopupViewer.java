7b3ca8e94e5dcc221733d4680c8b271c21002103
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-27 08:56:41 -0600

    added yes or no input method

--- src/popup/view/PopupViewer.java
+++ src/popup/view/PopupViewer.java
@@ -17,13 +17,35 @@ public class PopupViewer
 
 	}
 
+	/**
+	 * Makes a popup window to ask a question and return a response.
+	 * 
+	 * @param question
+	 *            The question that will be asked
+	 * @return Returns the response of the user.
+	 */
 	public String collectResponse(String question)
 	{
 		String response = "";
 		response = JOptionPane.showInputDialog(null, question);
+
+		return response;
+	}
+
 		
+     /** Makes a yes or no popup window
+	 * 
+	 * @param question
+	 *            The question to be asked
+	 * @return The constant value from JOptionPane of y/n/c
+	 */
+	public int collectOption(String question)
+	{
+		int response = 0;
+
+		response = JOptionPane.showConfirmDialog(null, question);
+
 		return response;
 	}
 
 }
-

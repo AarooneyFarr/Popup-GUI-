ef239c030b178c743ca8e082c61dda500505d67c
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-27 09:21:32 -0600

    added awesome check

--- src/popup/controller/PopupController.java
+++ src/popup/controller/PopupController.java
@@ -1,13 +1,32 @@
 package popup.controller;
 
+import popup.view.PopupViewer;
 
 public class PopupController
 {
+	private PopupViewer display;
+
+	public PopupController(){
+		display = new PopupViewer();
+	}
+	
 	public void start()
 	{
 		
+		awesome();
 		
 		
+	}
+	
+	public void awesome(){
+		String aaronAwesome = display.collectResponse("Is Aaron awesome?");
+		if(aaronAwesome.equalsIgnoreCase("yes")){
+			display.displayMessage("This is true");
+		}
+		else{
+			display.displayMessage("You are Wrong");
+			awesome();
+		}
 		
 	}
 }

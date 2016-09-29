f46f92520d23752d2dddb11e6efa546c4b45319a
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-09-29 08:25:12 -0600

    added int and double check

--- src/popup/controller/PopupController.java
+++ src/popup/controller/PopupController.java
@@ -17,23 +17,60 @@ public class PopupController
 		awesome("Username?", "Aaron Farr");
 
 		awesome("Password?", "Aaron rules");
-		
+
 	}
 
 	public void awesome(String question, String acceptableAnswer)
 	{
 		String aaronAwesome = display.collectResponse(question);
-		
-			
-	 if(aaronAwesome.equalsIgnoreCase(acceptableAnswer)){
+
+		if (aaronAwesome.equalsIgnoreCase(acceptableAnswer))
+		{
 			display.displayMessage("This is true");
 		}
-		
-		else if(!aaronAwesome.equalsIgnoreCase(acceptableAnswer))
+
+		else if (!aaronAwesome.equalsIgnoreCase(acceptableAnswer))
 		{
 			display.displayMessage("You are Wrong");
-			awesome(question,acceptableAnswer);
+			awesome(question, acceptableAnswer);
+		}
+		else
+		{
+			display.displayMessage("Error");
 		}
 	}
-	
+
+	private boolean isInteger(String input)
+	{
+		boolean isInt = false;
+		
+		try
+		{
+			int validInteger = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch (NumberFormatException Error)
+		{
+			display.displayMessage("You did not enter a valid integer");
+		}
+		return isInt;
+
+	}
+	private boolean isDouble(String input)
+	{
+		boolean isDouble = false;
+		
+		try
+		{
+			Double validDouble = Double.parseDouble(input);
+			isDouble = true;
+		}
+		catch (NumberFormatException Error)
+		{
+			display.displayMessage("You did not enter a valid number");
+		}
+		return isDouble;
+
+	}
+
 }
